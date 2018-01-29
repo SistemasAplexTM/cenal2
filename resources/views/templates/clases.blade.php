fd3b7740359d5422d6f0790cf7d4343bdef4fad6
unknown <duvierm24@gmail.com>
2018-01-29 14:25:29 -0500

    update

--- resources/views/templates/clases.blade.php
+++ resources/views/templates/clases.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     </div>
                     <div class="ibox-content">
-                        <div class="row m-b-sm m-t-sm">
+                        {{-- <div class="row m-b-sm m-t-sm">
                             <div class="col-md-1">
                                 <button type="button" id="loading-example-btn" class="btn btn-white btn-sm" ><i class="fa fa-refresh"></i> Actualizar</button>
                             </div>
@@ -22,19 +22,19 @@
                                 <div class="input-group"><input type="text" placeholder="Buscar clase" class="input-sm form-control"> <span class="input-group-btn">
                                     <button type="button" class="btn btn-sm btn-primary"> Buscar!</button> </span></div>
                             </div>
-                        </div>
+                        </div> --}}
 
                         <div class="project-list">
 
                             <table class="table table-hover" id="tbl-clases">
                                 <thead>
                                     <tr>
-                                        <th></th>
-                                        <th></th>
-                                        <th></th>
-                                        <th></th>
-                                        <th></th>
-                                        <th></th>
+                                        <th>Estado</th>
+                                        <th>Módulo</th>
+                                        <th>Salón</th>
+                                        <th>Progreso</th>
+                                        <th>Agregar</th>
+                                        <th>Acciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -58,11 +58,7 @@
                                                 </div>
                                         </td>
                                         <td class="project-people">
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
+                                            <a class="btn btn-primary" data-toggle="modal" data-target="#mdl-agregar-estudiante"><i class="fa fa-plus"></i> Agregar estudiante</a>
                                         </td>
                                         <td class="project-actions">
                                             <a href="#" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> Detalles </a>
@@ -89,10 +85,7 @@
                                                 </div>
                                         </td>
                                         <td class="project-people">
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
+                                            <a class="btn btn-primary" href=""><i class="fa fa-plus"></i> Agregar estudiante</a>
                                         <td class="project-actions">
                                             <a href="#" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> Detalles </a>
                                             <a href="#" class="btn btn-white btn-sm"><i class="fa fa-pencil"></i> Editar </a>
@@ -118,11 +111,7 @@
                                                 </div>
                                         </td>
                                         <td class="project-people">
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
-                                            <a href=""><img alt="image" class="img-circle" src="{{ asset('img/profile_small.jpg') }}"></a>
+                                            <a class="btn btn-primary" href=""><i class="fa fa-plus"></i> Agregar estudiante</a>
                                         </td>
                                         <td class="project-actions">
                                             <a href="#" class="btn btn-white btn-sm"><i class="fa fa-folder"></i> Detalles </a>
@@ -137,6 +126,26 @@
             </div>
         </div>
     </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="mdl-agregar-estudiante" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Agregar estudiante a la clase</h4>
+      </div>
+      <div class="modal-body">
+        <h3>Módulo Office</h3>
+        <small>Inicio: 01 enero 2018 - fin: 05 junio 2018</small>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-primary">Agregar</button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
 @push('scripts')
